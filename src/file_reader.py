901f3d40520d8b97f3fc6cadd6bcd2d5422d7cb7
Alexey Kultaev <kav7270@gmail.com>
2025-05-31 23:59:40 +1000

    update file_reader

--- src/file_reader.py
+++ src/file_reader.py
@@ -9,10 +9,9 @@ def reade_file(filepath: str = "data/operations.xlsx") -> pd.DataFrame | str:
     """
     try:
         df = pd.read_excel(filepath)
+        if df.empty:
+            return "Файл пустой"
         return df
 
     except Exception as e:
         return f"Error: {e}"
-
-
-# print(reade_file())
