475ba4f241e38fe174e0005880066af6b3212aa3
Alexey Kultaev <kav7270@gmail.com>
2025-06-02 00:21:49 +1000

    add funcs in module test_utils

---
+++ tests/test_utils.py
@@ -0,0 +1,39 @@
+from freezegun import freeze_time
+
+from src.utils import greetings
+
+
+@freeze_time("2025-06-01 07:00:00")
+def test_greetings_morning() -> None:
+    """
+    Тестирование функции приветствия
+    :return: None
+    """
+    assert greetings() == "Доброе утро"
+
+
+@freeze_time("2025-06-01 15:59:00")
+def test_greetings_day() -> None:
+    """
+    Тестирование функции приветствия
+    :return: None
+    """
+    assert greetings() == "Добрый день"
+
+
+@freeze_time("2025-06-01 22:55:00")
+def test_greetings_evening() -> None:
+    """
+    Тестирование функции приветствия
+    :return: None
+    """
+    assert greetings() == "Добрый вечер"
+
+
+@freeze_time("2025-05-01 03:00:00")
+def test_greetings_night() -> None:
+    """
+    Тестирование функции приветствия
+    :return: None
+    """
+    assert greetings() == "Доброй ночи"
