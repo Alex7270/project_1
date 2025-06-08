7908fe122f04f0b2ddad892eb8d2857c5c85de82
Alexey Kultaev <kav7270@gmail.com>
2025-06-08 17:42:59 +1000

    update utils.py file_reader.py

--- src/file_reader.py
+++ src/file_reader.py
@@ -5,13 +5,13 @@ def reade_file(filepath: str = "data/operations.xlsx") -> pd.DataFrame | str:
     """
     Читает содержимое файла
     :param filepath: str
-    :return: DataFrame | str
+    :return: pd.DataFrame | str
     """
     try:
         df = pd.read_excel(filepath)
         if df.empty:
             return "Файл пустой"
-        return df
+        return df.fillna(0)
 
     except Exception as e:
         return f"Error: {e}"
--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,9 @@
+import json
 from datetime import datetime
 
 from pandas import DataFrame
 
-from src import file_reader
-
-
-def clearing_data() -> DataFrame | str:
-    """
-    Очищает некорректные данные в столбцах 'Номер карты', 'Категория', 'MCC', заменяет отсутствующие данные на 0
-    :return: DataFrame
-    """
-    df: DataFrame | str = file_reader.reade_file()
-    if isinstance(df, DataFrame):
-        data = df.dropna(subset=["Номер карты", "Категория", "MCC"])
-        return data.fillna(0)
-    return "Неверный формат данных"
+from src.file_reader import reade_file
 
 
 def greetings() -> str:
@@ -32,4 +21,59 @@ def greetings() -> str:
     else:
         greeting = "Доброе утро"
 
-    return greeting
+    return json.dumps(greeting, ensure_ascii=False)
+
+
+# def format_date_columns() -> DataFrame:
+#     """
+#     Форматирует колонки с датами в DataFrame
+#     :return: DataFrame
+#     """
+#     data = reade_file()
+#     data["Дата операции"] = pd.to_datetime(
+#         data["Дата операции"], format="%d.%m.%Y %H:%M:%S", errors="coerce"
+#     )
+#     data["Дата платежа"] = pd.to_datetime(data["Дата платежа"], format="%d.%m.%Y", errors="coerce")
+#     return data
+
+
+def get_cards() -> str:
+    """
+    Группирует расходы по номерам карт
+    :return: DataFrame
+    """
+    data = reade_file()
+    if isinstance(data, DataFrame):
+        df = data.loc[(data["Сумма платежа"] < "0") & (data["Номер карты"] != 0) & (data["Статус"] == "OK")]
+        df_cards = df.groupby("Номер карты", as_index=False)["Сумма платежа"].sum()
+        df_cards = df_cards.assign(cashback=round(-df_cards["Сумма платежа"] / 100, 2))
+        df_cards = df_cards.rename(columns={"Номер карты": "last_digits", "Сумма платежа": "total_spent"})
+    else:
+        return ""
+    return json.dumps(df_cards.to_dict(orient="records"), ensure_ascii=False, indent=4)
+
+
+def top_transactions() -> str:
+    """
+    Сортирует транзакции по сумме платежа
+    :return: str
+    """
+    data = reade_file()
+    if isinstance(data, DataFrame):
+        df = (
+            data.loc[data["Статус"] == "OK"]
+            .sort_values(["Сумма платежа"], key=lambda x: abs(x), ascending=False)
+            .head(5)
+        )
+        df = df[["Дата операции", "Сумма платежа", "Категория", "Описание"]]
+        df = df.rename(
+            columns={
+                "Дата операции": "date",
+                "Сумма платежа": "amount",
+                "Категория": "category",
+                "Описание": "description",
+            }
+        )
+    else:
+        return ""
+    return json.dumps(df.to_dict(orient="records"), ensure_ascii=False, indent=4)
