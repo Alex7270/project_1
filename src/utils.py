777b9ebfa726d3bb84713a7fe6eac122104fe66b
Alexey Kultaev <kav7270@gmail.com>
2025-06-13 09:36:56 +1000

    update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,18 @@
 import json
+import os
 from datetime import datetime
 from typing import Any
 
 import pandas as pd
 from pandas import DataFrame
 
+from dotenv import load_dotenv
+
 from src.file_reader import reade_file
+
+import logger
+
+logger = logger.get_logger(__name__)   # 8 продвинутых возможностей модуля logging в Python, которые вы не должны пропустить
 
 
 def greetings() -> str:
@@ -82,3 +89,56 @@ def format_date_columns(data: DataFrame) -> Any:
 
 
 print(format_date_columns(reade_file()))
+
+# Загрузка переменных из .env-файла
+# load_dotenv()
+
+# Получение значения переменной API_KEY из .env-файла
+# api_key = os.getenv("API_KEY")
+
+# elif currency in ["USD", "EUR"]:
+# url = "https://api.apilayer.com/exchangerates_data/convert"
+# payload = {"amount": amount, "from": currency, "to": "RUB"}
+# headers = {"apikey": api_key}
+# try:
+#     response = requests.request("GET", url, headers=headers, params=payload, timeout=50)
+#     response.raise_for_status()
+#     result = response.json()
+# except requests.exceptions.RequestException as e:
+#     return f"Error: {e}"
+# else:
+#     return result.get("result")
+
+
+# def get_currency_rates() -> list|str:
+#     # logger.info("Начало работы функции по получению курсов валют")
+#     with open(settings, "r", encoding="utf-8") as file:
+#         currency_list = json.load(file)["user_currencies"]
+#         currency_rates = []
+#     for currency in currency_list:
+#         params = {"amount": 1, "to": "RUB", "from": currency}
+#         headers = {"apikey": api_token}
+#         try:
+#             response: Any = requests.get(api_url, headers=headers, params=params, timeout=50)
+#             status = response.raise_for_status()
+#             # logger.info(f"Попытка соединения с сервером, {status}")
+#         except requests.exceptions.RequestException as e:
+#             logger.error(f"Ошибка обращения к api {e}")
+#             return "Ошибка обращения к api"
+#         else:
+#             temp = {"currency": currency, "rate": round(response.json().get("info").get("rate"), 2)}
+#             currency_rates.append(temp)
+#             # logger.info("Успешное получение данных о курсе валют и формирование корректного JSON-ответа")
+#     return currency_rates
+#
+# def get_stock_rates() -> list:
+#     # logger.info("Начало работы функции по получению данных о котировках акций")
+#     with open(settings, "r", encoding="utf-8") as file:
+#         ticker_list = json.load(file)["user_stocks"]
+#     stock = []
+#     for ticker in ticker_list:
+#         temp = {"stock": ticker, "price": yf.Ticker(ticker).info["currentPrice"]}
+#         stock.append(temp)
+#         # logger.info("Успешное получение данных о котировках акций и формирование корректного JSON-ответа")
+#     return stock
+
