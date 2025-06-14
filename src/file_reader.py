dfc501af24fcfa6f5ff54397207792bd671ec993
Alexey Kultaev <kav7270@gmail.com>
2025-06-14 15:34:40 +1000

    fixed error

--- src/file_reader.py
+++ src/file_reader.py
@@ -2,8 +2,6 @@ import pandas as pd
 
 import logger
 
-logger = logger.get_logger(__name__)
-
 
 def reade_file(filepath: str = "data/operations.xlsx") -> pd.DataFrame | str | None:
     """
@@ -11,14 +9,11 @@ def reade_file(filepath: str = "data/operations.xlsx") -> pd.DataFrame | str | N
     :param filepath: str
     :return: pd.DataFrame | str
     """
-    logger.info("Чтение excel-файла")
     try:
         df = pd.read_excel(filepath)
         if df.empty:
             return "Файл пустой"
-        logger.info("excel-файл прочитан")
         return df.fillna(0)
 
     except Exception as e:
-        logger.error(f"Error: {e}")
-        return None
+        return f"Error: {e}"
