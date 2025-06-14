69fc0f4479f5bd3d3604f2860d93da276289c2fb
Alexey Kultaev <kav7270@gmail.com>
2025-06-14 11:57:39 +1000

    add logger in file_reader.py

--- src/file_reader.py
+++ src/file_reader.py
@@ -1,17 +1,24 @@
 import pandas as pd
 
+import logger
 
-def reade_file(filepath: str = "data/operations.xlsx") -> pd.DataFrame | str:
+logger = logger.get_logger(__name__)
+
+
+def reade_file(filepath: str = "data/operations.xlsx") -> pd.DataFrame | str | None:
     """
     Читает содержимое файла
     :param filepath: str
     :return: pd.DataFrame | str
     """
+    logger.info("Чтение excel-файла")
     try:
         df = pd.read_excel(filepath)
         if df.empty:
             return "Файл пустой"
+        logger.info("excel-файл прочитан")
         return df.fillna(0)
 
     except Exception as e:
-        return f"Error: {e}"
+        logger.error(f"Error: {e}")
+        return None
